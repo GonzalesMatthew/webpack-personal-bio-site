93c2482b1f6d72b6d185e0fb826a51c6c7f59cfa
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-03-05 21:21:18 -0600

    populated cards 70% of the way. going to fix the div containers and master container now

--- src/javascripts/helpers/createProjectCards.js
+++ src/javascripts/helpers/createProjectCards.js
@@ -8,16 +8,17 @@ const createProjectCards = (arrayOfObjects) => {
       domString += `
       <div class='card mb-3' style='max-width: 540px;'>
         <div class='row g-0'>
-          <div class='col-md-4'>
-            <img src='${object.screenshot}' alt='project screenshot'>
+          <div class='col-md-4' style='max-width: 540px;'>
+            <img src='${object.image}' alt='project screenshot'>
           </div>
           <div class='col-md-8'>
             <div class='card-body'>
               <h5 class='card-title'>${object.title}</h5>
               <p class='card-text'>${object.description}</p>
               <p class='card-text'><small class='text-muted'>${object.technologiesUsed}</small></p>
-              <p class='card-text'><small class='text-muted'>${object.url}</small></p>
+              <p class='card-text'><small class='text-muted'>${object.netlify}</small></p>
               <p class='card-text'><small class='text-muted'>${object.githubUrl}</small></p>
+              <p class='card-text'><small class='text-muted'>${object.loom}</small></p>
             </div>
           </div>
         </div>
--- src/javascripts/helpers/data/projects.js
+++ src/javascripts/helpers/data/projects.js
@@ -1,65 +1,58 @@
 const projects = [
   {
-    title: 'Cool Project',
-    screenshot: '',
-    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed:
-      'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+    title: 'Tamagotchi! App',
+    image: 'images/mgTamagotchi.png',
+    description: '', // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: 'HTML, Bootstrap, ',
     available: true,
-    netlify: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+    netlify: '',
+    githubUrl: 'https://github.com/GonzalesMatthew/ASSIGNMENT-Tamagotchi',
     loom: '',
-    image: '',
-
-  },
-
-  {
-    title: 'Cool Project',
-    screenshot: '',
-    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed:
-      'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-    available: true,
-    netlify: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
-    loom: '',
-    image: '',
+    type: 'personal',
   },
   {
-    title: 'Cool Project',
-    screenshot: '',
-    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed:
-      'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+    title: 'Bear Tracker App',
+    image: 'images/mgBearWatcherApp.png',
+    description: '', // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
     available: true,
-    netlify: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+    netlify: 'https://mg-bear-tracking-app.netlify.app/',
+    githubUrl: 'https://github.com/GonzalesMatthew/ASSIGNMENT-Bear-Watcher',
     loom: '',
-    image: '',
+    type: 'personal',
   },
   {
-    title: 'Cool Project',
-    screenshot: '',
-    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed:
-      'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+    title: 'Sorting Hat',
+    image: 'images/mgSortingHat.png',
+    description: '', // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
     available: true,
-    netlify: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+    netlify: '',
+    githubUrl: 'https://github.com/GonzalesMatthew/sorting-hat-webpack',
     loom: '',
-    image: '',
+    type: 'personal',
   },
   {
-    title: 'Cool Project',
-    screenshot: '',
-    description: 'This is the best project', // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed:
-      'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-    available: true,
-    netlify: 'https://github.com/nss-evening-cohort-8/js-part-deux', // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: 'https://github.com/nss-evening-cohort-8/js-part-deux',
+    title: 'GitHub Clone - Placeholder',
+    image: 'images/code_image.jpeg',
+    description: '', // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+    available: false,
+    netlify: '',
+    githubUrl: '',
     loom: '',
-    image: '',
+    type: 'group',
+  },
+  {
+    title: 'Almost Amazon',
+    image: 'images/code_image.jpeg',
+    description: '', // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+    available: false,
+    netlify: '',
+    githubUrl: 'https://github.com/GonzalesMatthew/mg-almost-amazon-inclass',
+    loom: '',
+    type: 'personal',
   },
 ];
 
